e47ab06c66163ac6dde97849f186b7997ab7925c
JUX84 <mehdi.mhiri@gmail.com>
2015-03-11 17:15:10 +0100

    fix tweet processing

--- Controller.php
+++ Controller.php
@@ -2,7 +2,7 @@
 
 class Controller
 {
-    static function processTweet(&$tweet)
+    static function processTweet($tweet)
     {
         $tweet = str_replace('RT', '<b>RT</b>', $tweet);
         $tweet = preg_replace_callback(
@@ -12,10 +12,18 @@ class Controller
             },
             $tweet
         );
+        $tweet = preg_replace_callback(
+            '/#[A-Z0-9_]*/i',
+            function ($matches) {
+                return '<a href="/?tag=' . substr($matches[0], 1) . '">' . $matches[0] . '</a>';
+            },
+            $tweet
+        );
         $tweet = preg_replace(
             '/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/',
             '<a href="$0">$0</a>',
             $tweet
         );
+        return $tweet;
     }
 }
