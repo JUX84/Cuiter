729772d66f5abb0f468f3c45ff46492ac31f080e
JUX84 <mehdi.mhiri@gmail.com>
2015-03-11 17:30:24 +0100

    change background for rt

--- view/profile.php
+++ view/profile.php
@@ -36,7 +36,7 @@
         foreach($tweets as $tweet) {
         ?>
             <h4><?=$tweet['user']['name']?> <span class="date">(<?=date('d/m/Y, H:i:s', strtotime($tweet['created_at']))?>)</span></h4>
-            <p<?=(substr($tweet, 0, 2) == 'RT' ? ' class="rt"' : '')?>><?=Controller::processTweet($tweet['text'])?></p>
+            <p<?=(substr($tweet, 0, 2) == 'RT' ? ' class="retweet"' : '')?>><?=Controller::processTweet($tweet['text'])?></p>
         <?php
         }
         ?>
