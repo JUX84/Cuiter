a7bedf36d2b0f50ccd9745e49047f806455aff18
JUX84 <mehdi.mhiri@gmail.com>
2015-03-11 14:57:49 +0100

    check vars before printing them

--- view/profile.php
+++ view/profile.php
@@ -4,10 +4,30 @@
          background-color: #<?=$user['profile_background_color']?>;
          color: #<?=$user['profile_text_color']?>;
          ">
-    <img src="<?=$user['profile_image_url']?>" />
+    <?php
+    if(isset($user['profile_image_url']) && !empty($user['profile_image_url'])) {
+        ?>
+        <img src="<?= $user['profile_image_url'] ?>"/>
+    <?php
+    }
+    ?>
     <h1><?=$user['name']?></h1>
-    <p class="lead"><?=$user['description']?></p>
-    <p><a class="btn btn-lg btn-success" href="#" role="button"><?=$user['url']?></a></p>
+    <h3><a href="#">@<?=$user['screen_name']?></a></h3>
+    <?php
+    if(isset($user['description']) && !empty($user['description'])) {
+        ?>
+        <p class="lead"><?=$user['description']?></p>
+    <?php
+    }
+    ?>
+    <?php
+    if(isset($user['url']) && !empty($user['url'])) {
+        ?>
+        <p><a class="btn btn-lg btn-success" href="#" role="button"><?=$user['url']?></a></p>
+    <?php
+    }
+    ?>
+
 </div>
 
 <div class="row marketing">
