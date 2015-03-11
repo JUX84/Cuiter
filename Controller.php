6500476444869541db41cc020f4608885db1176a
JUX84 <mehdi.mhiri@gmail.com>
2015-03-11 17:55:54 +0100

    debug

--- Controller.php
+++ Controller.php
@@ -13,8 +13,8 @@ class Controller
                 $tweet = substr($tweet, $url['url'], '<a href="'.$url['url'][0].'">'.$url['display_url'].'</a>', $url['indices'][1]-$url['indices'][1]);
             /*foreach($entity['hashtags'] as $hashtag)
                 $tweet = substr($tweet, '#'.$hashtag['url'], $hashtag['indices'][0], $hashtag['indices'][1]-$hashtag['indices'][1]);*/
-            foreach($entity['user_mentions'] as $user)
-                $tweet = substr($tweet, '@'.$user['screen_name'], '<a href="'.$user['screen_name'].'">@'.$user['screen_name'].'</a>', $user['indices'][1]-$user['indices'][1]);
+            /*foreach($entity['user_mentions'] as $user)
+                $tweet = substr($tweet, '@'.$user['screen_name'], '<a href="'.$user['screen_name'].'">@'.$user['screen_name'].'</a>', $user['indices'][1]-$user['indices'][1]);*/
         }
         return $tweet;
     }
